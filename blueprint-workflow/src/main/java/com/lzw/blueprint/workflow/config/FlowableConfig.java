442b09c60e062356249a8cd459672c617568643a
liu-zw@primeton.com <liu-zw@primeton.com>
2026-09-16 16:56:30 +0800

    fix(workflow): gate FlowableConfig on blueprint.workflow.enabled

--- blueprint-workflow/src/main/java/com/lzw/blueprint/workflow/config/FlowableConfig.java
+++ blueprint-workflow/src/main/java/com/lzw/blueprint/workflow/config/FlowableConfig.java
@@ -2,6 +2,7 @@ package com.lzw.blueprint.workflow.config;
 
 import org.flowable.engine.ProcessEngine;
 import org.flowable.spring.SpringProcessEngineConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -9,6 +10,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import javax.sql.DataSource;
 
 @Configuration
+@ConditionalOnProperty(name = "blueprint.workflow.enabled", havingValue = "true", matchIfMissing = true)
 public class FlowableConfig {
 
     @Bean(destroyMethod = "close")
